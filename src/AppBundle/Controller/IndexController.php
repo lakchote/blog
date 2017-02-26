059c618845b541d418692166d8d4916ef475d9ec
Lucien Akchoté <l.akchote@gmail.com>
2017-02-26 13:42:23 +0100

    Closes #1, template de base version statique

---
+++ src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class IndexController extends Controller
+{
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('index_controller/index.html.twig');
+    }
+}
