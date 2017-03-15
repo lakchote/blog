73ca145d582ade2e3a7701be2b25ff5b9163962c
Lucien Akchoté <l.akchote@gmail.com>
2017-03-15 13:54:35 +0100

    Ajout contrainte Length max pour la propriété contenu de l'entité Commentaires

--- src/AppBundle/Entity/Commentaires.php
+++ src/AppBundle/Entity/Commentaires.php
@@ -33,7 +33,7 @@ class Commentaires
     /**
      * @ORM\Column(type="text")
      * @Assert\NotBlank()
-     * @Assert\Length(min="15")
+     * @Assert\Length(min="15", max="1000")
      */
     private $contenu;
 
